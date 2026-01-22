4c9712bb7d0c5629d81d830d1305b9b12da4497b
Amin Omidzade <amin.omidzade@gmail.com>
2026-01-22 21:25:18 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Omidzadehnik
+# Jan 22 2026
+
+# Import Modules
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from scipy import stats
+from scipy.spatial import distance
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
